fcb95d918d321dfacb5333580eef42793dc22d2a
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2023-04-19 16:26:35 +0000

    extract the steps to it's own file

---
+++ components/steps/Steps.tsx
@@ -0,0 +1,18 @@
+// components/steps/Steps.tsx
+import React from "react";
+
+export const SourceStep = () => {
+  return <h1>Source Step Content</h1>;
+};
+
+export const TargetStep = () => {
+  return <h1>Target Step Content</h1>;
+};
+
+export const MergeStep = () => {
+  return <h1>Merge Step Content</h1>;
+};
+
+export const ResultStep = () => {
+  return <h1>Result Step Content</h1>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,19 @@ import {
   Paper,
   Grid,
 } from "@mui/material";
+import {
+  SourceStep,
+  TargetStep,
+  MergeStep,
+  ResultStep,
+} from "../components/steps/Steps";
 
-const steps = ["Source", "Target", "Merge", "Result"];
+const steps = [
+  { label: "Source", component: <SourceStep /> },
+  { label: "Target", component: <TargetStep /> },
+  { label: "Merge", component: <MergeStep /> },
+  { label: "Result", component: <ResultStep /> },
+];
 
 const HorizontalStepper = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -43,43 +54,33 @@ const HorizontalStepper = () => {
         }}
       >
         <Stepper activeStep={activeStep} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
+          {steps.map((step) => (
+            <Step key={step.label}>
+              <StepLabel>{step.label}</StepLabel>
             </Step>
           ))}
         </Stepper>
         {/* Add your content related to each step */}
         <Box sx={{ flexGrow: 1, marginTop: 2 }}>
-          {activeStep === steps.length ? (
-            <div>
-              {/* Add your final step content */}
-              <h1>All steps completed</h1>
-            </div>
-          ) : (
-            <div>
-              {/* Add your content based on the active step */}
-              <h1>{steps[activeStep]}</h1>
-              <Grid container justifyContent="space-between">
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  disabled={activeStep === 0}
-                  onClick={handleBack}
-                >
-                  Back
-                </Button>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNext}
-                  disabled={activeStep === steps.length - 1}
-                >
-                  {activeStep === steps.length - 1 ? "Finish" : "Next"}
-                </Button>
-              </Grid>
-            </div>
-          )}
+          {steps[activeStep].component}
+          <Grid container justifyContent="space-between">
+            <Button
+              variant="outlined"
+              color="primary"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleNext}
+              disabled={activeStep === steps.length - 1}
+            >
+              {activeStep === steps.length - 1 ? "Finish" : "Next"}
+            </Button>
+          </Grid>
         </Box>
       </Paper>
     </Box>
